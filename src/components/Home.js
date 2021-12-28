8300025d4b41655d10139290318285cbb6b0eee3
isaac8069 <isaac8069@gmail.com>
2021-12-28 18:55:44 -0500

    client is working

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+const Home = (props) => {
+    // const { msgAlert, user } = props
+    console.log('props in home', props)
+
+    return (
+        <>
+            <h2>Apartment Buddy</h2>
+        </>
+    )
+}
+
+export default Home
